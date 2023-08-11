3b2725a52b8ef049232d790229bb861e3e983648
jpedro002 <joaopsilvavolei@gmail.com>
2023-08-11 09:21:30 -0300

    update: icons

--- App.tsx
+++ App.tsx
@@ -1,62 +1,71 @@
 import React, { useState } from "react";
 import { Pressable, Text, TextInput, View } from "react-native";
-import Icon from "react-native-vector-icons/FontAwesome5"
-
+import Icon from "react-native-vector-icons/FontAwesome5";
+import Fontisto from "react-native-vector-icons/Fontisto";
 
 export default function App() {
-
-  const [SwichEye , setSwichEye] = useState(true)
+  const [SwichEye, setSwichEye] = useState(true);
   const handleCLickEye = () => {
-    setSwichEye(SwichEye => !SwichEye)
-  } 
+    setSwichEye((SwichEye) => !SwichEye);
+  };
 
   return (
     <View className="w-screen h-screen items-center justify-center gap-4  ">
       <Text className="text-3xl pb-16   ">login</Text>
 
-      <View className="relative flex-row w-11/12 items-center border">
-
+      <View className="relative flex flex-col justify-center w-11/12  ">
         <TextInput
           className="text-black h-20 w-full rounded-full pl-12 bg-slate-200  placeholder:text-xl  "
-          placeholder="useless placeholder"
+          placeholder="Email"
         />
-        
+        <Fontisto
+          name="email"
+          size={20}
+          style={{ position: "absolute",
+                  left: 15,
 
+                }}
+          color="#999"
+        />
       </View>
-      <View className="relative flex-row w-11/12 items-center border">
-
+      <View className="relative flex flex-col justify-center w-11/12  ">
         <TextInput
           className="text-black h-20 w-full rounded-full pl-12 bg-slate-200  placeholder:text-xl  "
-          placeholder="useless placeholder"
+          placeholder="Senha"
+        />
+        <Fontisto
+          name="locked"
+          size={20}
+          style={{ position: "absolute",
+                  left: 15,
+
+                }}
+          color="#999"
         />
         <Pressable
           onPress={handleCLickEye}
-          className="p-10 bg-black absolute"
+          className="p-10 flex justify-center right-0  absolute"
         >
-          <Icon name={SwichEye ? "eye" : "eye-slash"} size={20} 
-          color="#4F8EF7"
-                style={{
-                  margin: 10,
-                  position: 'absolute',
-                  right: 10,
-                }}
+
+          <Icon
+            name={SwichEye ? "eye" : "eye-slash"}
+            size={20}
+            color="#999"
+            style={{
+              margin: 10,
+              position: "absolute",
+              right: 10,
+            }}
           />
         </Pressable>
-        
-
       </View>
-      
-      
-     
 
       <Pressable className="bg-green-600 w-3/5 h-20 rounded-full p-3 justify-center items-center active:bg-slate-700">
-        <Text className="text-white ">oiaiiidi</Text>
+        <Text className="text-white ">Entrar</Text>
       </Pressable>
       <Pressable className="bg-green-600 w-3/5 h-20 rounded-full p-3 justify-center items-center active:bg-slate-700">
-        <Text className="text-white ">oiaiiidi</Text>
+        <Text className="text-white ">Criar Conta</Text>
       </Pressable>
-
-
     </View>
   );
 }
